1670294eb2f3b767c932cb505a80a1db5946a32c
Haley Fitzgerald <haleyfitzgerald27@gmail.com>
2025-04-23 10:58:35 -0400

    when you click buy now button in the cart, it opens the wizard frame - not connected w/ functionality yet though

--- src/edu/psu/ist/cartmanagement/controller/CartController.java
+++ src/edu/psu/ist/cartmanagement/controller/CartController.java
@@ -11,6 +11,7 @@ import javax.swing.SwingUtilities;
 import edu.psu.ist.cartmanagement.model.CartManager;
 import edu.psu.ist.cartmanagement.util.CartObserver;
 import edu.psu.ist.cartmanagement.view.CartContentsView;
+import edu.psu.ist.paymentmanagement.view.WizardFrame;
 import edu.psu.ist.productmanagement.controller.ProductListingController;
 import edu.psu.ist.productmanagement.controller.ProductPageController;
 import edu.psu.ist.productmanagement.model.Pricing;
@@ -91,7 +92,9 @@ public class CartController implements CartObserver {
             if (cart.isEmpty()) {
                 JOptionPane.showMessageDialog(view.getBasePanel(), "Cart is empty! Add products before purchasing.");
             } else {
-                JOptionPane.showMessageDialog(view.getBasePanel(), "Buy Now Button Pressed");
+//                JOptionPane.showMessageDialog(view.getBasePanel(), "Buy Now Button Pressed");
+                new WizardFrame();
+                view.setVisible(false);
             }
         });
         view.getBackButton().addActionListener(e -> {
