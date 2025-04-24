e1874c70d3b4109283e8ad3394b9089e5bc4a059
Haley Fitzgerald <haleyfitzgerald27@gmail.com>
2025-04-24 19:28:56 -0400

    UI step 3 panel

--- src/edu/psu/ist/paymentmanagement/view/Step3Panel.java
+++ src/edu/psu/ist/paymentmanagement/view/Step3Panel.java
@@ -1,6 +1,7 @@
 package edu.psu.ist.paymentmanagement.view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Step3Panel extends WizardStepPanel {
 
@@ -8,15 +9,45 @@ public class Step3Panel extends WizardStepPanel {
     private JLabel instructionLabel;
     private JLabel orderLabel;
     private JButton printReceiptButton;
+    private JPanel basePanel;
+    private JPanel headerPanel;
+    private JPanel orderPanel;
+
+
     public Step3Panel() {
-        instructionLabel = new JLabel("Step 3: Confirm & Print Receipt");
+        setUpBasePanel();
+        setUpHeaderPanel();
+        setUpOrderPanel();
+    }
+
+    private void setUpBasePanel(){
+        basePanel = new JPanel();
+        basePanel.setLayout(new BorderLayout());
+        add(basePanel);
+    }
+
+    private void setUpHeaderPanel(){
+        headerPanel = new JPanel();
         backButton = new JButton("Back");
+        instructionLabel = new JLabel("Step 3: Confirm & Print Receipt");
+
+        headerPanel.add(backButton);
+        headerPanel.add(instructionLabel);
+
+        basePanel.add(headerPanel, BorderLayout.NORTH);
+    }
+
+    private void setUpOrderPanel(){
+        orderPanel = new JPanel();
         printReceiptButton = new JButton("Confirm Order");
         orderLabel = new JLabel();
 
-        add(backButton);
-        add(instructionLabel);
-        add(orderLabel);
+        orderLabel.setPreferredSize(new Dimension(300, 100));
+
+        orderPanel.add(orderLabel);
+        orderPanel.add(printReceiptButton);
+
+        basePanel.add(orderPanel, BorderLayout.CENTER);
     }
 
     @Override
@@ -28,5 +59,17 @@ public class Step3Panel extends WizardStepPanel {
     public JButton getBackButton() {
         return backButton;
     }
+
+    public JLabel getInstructionLabel() { return instructionLabel; }
+
+    public JLabel getOrderLabel() { return orderLabel; }
+
+    public JButton getPrintReceiptButton() { return printReceiptButton; }
+
+    public JPanel getBasePanel() { return basePanel; }
+
+    public JPanel getHeaderPanel() { return headerPanel; }
+
+    public JPanel getOrderPanel() { return orderPanel; }
 }
 
